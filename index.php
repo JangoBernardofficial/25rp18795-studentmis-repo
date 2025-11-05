7d19b85332f443f56e59382f5be524c0ee4fa012
bernard <bernardniringiyimana@gmail.com>
2025-11-05 11:20:51 +0200

    index page is created

---
+++ index.php
@@ -0,0 +1,20 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>25rp18795 STUDENT MIS</title>
+</head>
+<body>
+    <h1>Welcome to Students MIS</h1>
+    <a href="register.php">Go to Registration Page</a>
+</body>
+</html>
+
+
+<?php
+
+?>
